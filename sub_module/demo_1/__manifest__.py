d0cfa2aea14000c685e64b7d0ef23fcda1414787
tysamncaweb <soporte.innova2129@gmail.com>
2020-03-05 10:58:15 -0400

    correcion de factura

---
+++ sub_module/demo_1/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ejercicio nro 1",
+
+    'summary': """ejercicio nro 1""",
+
+    'description': """
+       Botón "Crear un boton en el menu de ajustes.
+    """,
+    'version': '2.0',
+    'author': 'Darrell Sojo. Ing 1',
+    'category': 'Tools',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [        
+        'vista_view.xml'
+    ],
+    'application': True,
+}
